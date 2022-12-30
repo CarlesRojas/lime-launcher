1aadf1fad50acc106641cf84b6a3a115d9bf9fc1
CarlesRojas <carles.rojas@mundimoto.com>
2022-12-30 23:11:40 +0100

    clear text icon

--- app/src/main/java/app/pinya/lime/DrawerAdapter.kt
+++ app/src/main/java/app/pinya/lime/DrawerAdapter.kt
@@ -1,13 +1,16 @@
 package app.pinya.lime
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -19,6 +22,8 @@ class DrawerAdapter(context: Context, state: State, layout: ViewGroup) :
     private val layout: ViewGroup
 
     private lateinit var searchBar: EditText
+    private var searchBarText: String = ""
+
 
     init {
         this.context = context
@@ -28,11 +33,43 @@ class DrawerAdapter(context: Context, state: State, layout: ViewGroup) :
         initSearchBar()
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initSearchBar() {
-        this.searchBar = layout.findViewById(R.id.drawerSearchBar)
+        searchBar = layout.findViewById(R.id.drawerSearchBar)
+
         searchBar.setOnFocusChangeListener { _, focus ->
             print(focus)
         }
+
+        searchBar.doAfterTextChanged {
+            if (it.toString() == "") hideClearText() else showClearText()
+            searchBarText = it.toString()
+        }
+
+        searchBar.setOnTouchListener { view, event ->
+
+            if (event.action == MotionEvent.ACTION_UP && searchBarText != "") {
+                val padding: Int = searchBar.paddingRight * 2
+                val iconWidth: Int = searchBar.compoundDrawables[2].bounds.width()
+
+                if (event.rawX >= searchBar.right - iconWidth - padding) clearText()
+            }
+
+            view.performClick()
+        }
+
+    }
+
+    private fun clearText() {
+        searchBar.text.clear()
+    }
+
+    private fun showClearText() {
+        searchBar.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.close_line, 0)
+    }
+
+    private fun hideClearText() {
+        searchBar.setCompoundDrawablesWithIntrinsicBounds(R.drawable.search_line, 0, 0, 0)
     }
 
     fun showKeyboard() {
