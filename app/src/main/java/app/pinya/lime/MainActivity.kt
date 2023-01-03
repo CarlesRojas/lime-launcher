bf8ae3d71c45f969059afb9852d7c3cd7b77173f
CarlesRojas <carles.rojas@mundimoto.com>
2023-01-03 14:52:00 +0100

    test global styles

--- app/src/main/java/app/pinya/lime/MainActivity.kt
+++ app/src/main/java/app/pinya/lime/MainActivity.kt
@@ -12,11 +12,14 @@ class MainActivity : Activity() {
     private lateinit var customPageAdapter: CustomPagerAdapter
     private lateinit var viewPager: ViewPager
 
+    private var currentTextIsBlack: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        state = State(this)
+        applyStyle()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        state = State(this)
 
         linkAdapters()
     }
@@ -44,11 +47,22 @@ class MainActivity : Activity() {
     }
 
     override fun onResume() {
+        applyStyle()
+
         super.onResume()
 
         state.fetchInstalledAppsAgain()
         state.hideMenu()
         viewPager.currentItem = 0
         customPageAdapter.onResume()
+    }
+
+    private fun applyStyle() {
+        val blackTextValue = state.getData(DataKey.BLACK_TEXT, false)
+        if (blackTextValue != currentTextIsBlack) {
+            theme.applyStyle(if (blackTextValue) R.style.BlackText else R.style.WhiteText, true)
+            currentTextIsBlack = blackTextValue
+            recreate()
+        }
     }
 }
